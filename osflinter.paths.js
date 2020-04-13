cf1a9d26f84aeea8276bc73471e1634f96d5eea1
RaresVelnita <62063411+RaresVelnitaOSF@users.noreply.github.com>
2020-04-13 17:07:22 +0300

    OSF Linter
    Added OSF Linter with CRA v2

---
+++ osflinter.paths.js
@@ -0,0 +1,12 @@
+module.exports.JS = [
+    "app_storefront_training/**/*.js",  // include all JS files from the app_demo cartridge
+    "!app_storefront_training/cartridge/client/default/js/specific-file.js" // exclude this JavaScript file using a path pattern that begins with "!"
+];
+
+module.exports.SCSS = [
+    "app_storefront_training/cartridge/client/*/css/**/*.scss"
+];
+
+module.exports.ISML = [
+    "app_storefront_training/cartridge/templates/**/*.isml"
+];
