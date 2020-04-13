b63f29fe4f1dce339de29d6c47e152c4abfd2112
RaresVelnita <62063411+RaresVelnitaOSF@users.noreply.github.com>
2020-04-13 12:28:04 +0300

    INDTR 20901 Student Guide
    Implemented some from the student guide

---
+++ app_storefront_training/cartridge/controllers/Call.js
@@ -0,0 +1,18 @@
+"use strict";
+
+var guard = require("app_storefront_controllers/cartridge/scripts/guard");
+var ISML = require("dw/template/ISML"); 
+
+function start() {
+    var parameterMap = request.httpParameterMap.param.stringValue;
+
+    if (parameterMap) {
+        ISML.renderTemplate("notEmpty", {
+            result: parameterMap
+        });      
+    } else {
+        ISML.renderTemplate("empty");
+    }
+};
+
+exports.Start = guard.ensure(["get"], start);
